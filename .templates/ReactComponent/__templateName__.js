503e46f5302c86853fd2555b11f7a5dc199831e1
CRW <chris.wilhite@datastax.com>
2021-12-10 16:37:52 -0500

    remove node_modules

---
+++ .templates/ReactComponent/__templateName__.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const __templateName__ = () => {
+  return <div>__templateName__</div>;
+};
+
+__templateName__.propTypes = {};
+
+__templateName__.defaultProps = {};
+
+export default __templateName__;
---
+++ .templates/ReactComponent/__templateName__.stories.js
@@ -0,0 +1,12 @@
+import React from "react";
+import __templateName__ from "./__templateName__";
+
+export default {
+  title: "Components/__templateName__",
+  component: __templateName__,
+};
+
+const Template = (args) => <__templateName__ {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
