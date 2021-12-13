c18e356635ec54f0473d741e45e34b644186005d
CRW <chris.wilhite@datastax.com>
2021-12-13 15:26:45 -0500

    remove templates

--- .templates/ReactComponent/__templateName__.test.js
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import __templateName__ from "./__templateName__";
-
-describe("__templateName__", () => {
-  it("renders without crashing", () => {
-    render(<__templateName__ />);
-  });
-});
